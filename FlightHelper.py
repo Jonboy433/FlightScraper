9f6bce598564914a85b170b65fc43d5e5436a785
John Greco <jonboy433@hotmail.com>
2018-03-02 14:10:55 -0500

    First set of changes ready for commit

---
+++ FlightHelper.py
@@ -0,0 +1,258 @@
+from collections import OrderedDict
+import json
+from datetime import datetime
+import math
+import logging
+
+logging.basicConfig(level=logging.DEBUG, filename='app.log', format='%(asctime)s: %(levelname)s: %(message)s')
+
+#in order to query for flights we must have a list to go against
+currentFlightList = {}
+
+def setAvailableFlightList(availableFlights):
+    global currentFlightList
+
+    currentFlightList = availableFlights
+    logging.debug('setAvailableFlight: setting new flight list')
+    logging.debug('setAvailableFlight: ' + str(len(currentFlightList)) + ' outbound/inbound flights are currently available')
+    logging.debug('setAvailableFlight: keys: ' + str(currentFlightList.keys()))
+
+def __getCheapestOutboundFlight(outboundList):
+    return outboundList[0]
+
+def __getCheapestInboundFlight(inboundList):
+    return inboundList[0]
+
+def __compileFlightSegments(flightIDs):
+    # flightIDs is a list
+    # we need to create a new flight dict... outbound key set to any match we find and inbound the same
+    flights = {
+        'flights' : []
+    }
+
+    for id in flightIDs:
+        newFlight = {}
+        logging.debug('flightSegments: Looking for an outbound flight with id: ' + id['outbound']['id'])
+        if (id['outbound']['id']) in currentFlightList.keys():
+            logging.debug('found a flight matching this id... adding to flight list')
+            newFlight['outbound'] = currentFlightList.get(id['outbound']['id'])
+            newFlight['outbound']['one_way_price'] = id['outbound']['one_way_price']
+        if (id['inbound']['id']) in currentFlightList.keys():
+            logging.debug('found a flight matching this id... adding to flight list')
+            newFlight['inbound'] = currentFlightList.get(id['inbound']['id'])
+            newFlight['inbound']['one_way_price'] = id['inbound']['one_way_price']
+
+        # Total the total cost for this roundtrip
+        newFlight['round_trip_cost'] = newFlight['outbound']['one_way_price'] + newFlight['inbound']['one_way_price']
+
+        flights['flights'].append(newFlight)
+
+    return flights
+
+def getCheapestRoundTripFlights(outboundList, inboundList, numFlights=5):
+    #
+    # Let's use 5 as an example. The first 5 items in the outbound list will have the 5 cheapest one-way outbounds
+    # We can make a list as such: Get a list of those 5 flights and then add the first inbound to each
+    # So Outbound1 + Inbound1 and then Outbound2 + Inbound2 and so on... those are the five cheapest flights
+    # that match our criteria. Still - all we have are flight IDs and prices so we need the actual flight info
+    #
+    # numFlights = number of cheapest flights to return. Default is 5 cheapest
+    #Using an OrderedDict here to preserve the order of the flights for display purposes
+
+    outboundFlights = outboundList[:numFlights]
+    inboundFlights = inboundList[0]
+    global currentFlightList
+
+    flights = []
+
+    for flight in range(0, numFlights):
+        newFlight = {}
+        logging.debug('Trying to add this flight: ' + outboundFlights[flight]['flight'])
+        newFlight['outbound'] = {
+            'id' : outboundFlights[flight]['flight'] ,
+            'one_way_price' : outboundFlights[flight]['one_way_price']
+        }
+
+        newFlight['inbound'] = {
+            'id' : inboundFlights['flight'] ,
+            'one_way_price' : inboundFlights['one_way_price']
+        }
+
+        logging.debug('getCheapest: adding ' + str(newFlight) + ' to final list')
+        flights.append(newFlight)
+
+    # At this stage flights is a list of dicts that only has inbound and outbound flight IDs
+    # We need to write a method that will accept this list as an input and then return the full flights
+
+    return __compileFlightSegments(flights)
+
+def getCheapestReturnFlightID(inbound_flights):
+    #Skiplagged data is already sorte by cheapest first so we can grab the first one
+    return inbound_flights[0]['flight']
+
+def getCheapestReturnFlightsID(inbound_flights, numFlights=5):
+    #This method allows the caller to determine how many inbound flights to return. Default is 5
+    #Returns a list of flights
+    cheapestReturnFlights = []
+    for flight in inbound_flights:
+        cheapestReturnFlights.append(flight['flight'])
+    return inbound_flights[:numFlights]
+
+def getFlightByID(flightList, id):
+    #all_flights = complete list
+    #flight ID = 7 digit alpha; must be string
+    return flightList[id]
+
+def getFlightsByID(all_flights, selectedFlights):
+    #selectedFlights is a list of flight IDs
+    #this method should return a dict of flights
+    print('getFlightsByID: processing list: ' + str(selectedFlights))
+    if(not isinstance(selectedFlights, list)):
+        raise ValueError('selectedFlights arg must be of type list')
+    elif (not isinstance(all_flights, dict)):
+        raise ValueError('all_flights arg must be of type dict')
+    else:
+        flightList = {}
+        missingFlights = []
+        flightList['foundAllFlights'] = 'true'
+        for flightId in selectedFlights:
+            try:
+                flightList[flightId] = getFlightByID(all_flights, flightId)
+            except KeyError:
+                #Update flag to let user know we could not find everything
+                flightList['foundAllFlights'] = 'false'
+                #Add flight id to missingFlights list
+                missingFlights.append(flightId)
+                flightList['missingFlightIDs'] = missingFlights
+        return flightList
+
+def getTotalNumFlights(all_flights):
+    return str(len(all_flights))
+
+def sanitizeMultiStopLongFlights(flightList, maxDuration=21600):
+    """This function takes a dict (all_flights) and removes extraneous data
+       First, remove any flight with more than one connection flight. Second, filter out flights that total more than 6 hours of total flying time"""
+    ## TODO: add type checking; this method should only accept dictionaries
+
+    modifiedLists = []
+
+    #Remove flights with more than 1 stop and with a total duration of 5 or more hours
+    logging.debug('sanitizeMultiStopLongFlights: ' + 'Sanitizing list to remove multi-stop and long duration flights...')
+    sanitizedList = {k:v for k,v in flightList.items() if v['count'] <= 2 and v['duration'] <= maxDuration }
+    logging.debug('sanitizeMultiStopLongFlights: ' + 'Size of result set reduced from ' + str(len(flightList)) + ' to ' + str(len(sanitizedList)))
+
+    #We need to use this list to filter the separate outbound/inbound lists to make sure we arent pulling them in any future queries
+    discardedList = {k:v for k,v in flightList.items() if v['count'] > 2 or v['duration'] > maxDuration }
+    logging.debug('sanitizeMultistopLongFlights: ' + 'Size of discarded list is ' + str(len(discardedList)))
+
+    modifiedLists.append(sanitizedList)
+    modifiedLists.append(discardedList)
+    return modifiedLists
+
+def sanitizeInOutbound(flightsInbound, flightsOutbound, flightsToRemove):
+
+    filteredInbound = []
+    filteredOutbound = []
+    logging.debug('sanitizeInOutBound: Removing flights that have too many connections or are too long')
+    for flight in flightsInbound:
+        if flight['flight'] not in flightsToRemove.keys():
+            logging.debug('sanitizeInOutbound: flight ' + str(flight['flight']) + ' is being added to the inbound list')
+            filteredInbound.append(flight)
+    for flight in flightsOutbound:
+        if flight['flight'] not in flightsToRemove.keys():
+            logging.debug('sanitizeInOutbound: flight ' + str(flight['flight']) + ' is being added to the outbound list')
+            filteredOutbound.append(flight)
+
+    logging.debug('sanitizeInOutBoud: ' + str(len(filteredInbound)) + ' flights OK for inbound')
+    logging.debug('sanitizeInOutBoud: ' + str(len(filteredOutbound)) + ' flights OK d for outbound')
+
+    #return a list [filteredInboud, filteredOutbound]
+    return filteredInbound, filteredOutbound
+
+
+def removeDataKeyFromFlights(flightList):
+    """ There is a key called "data" in the outbound flight list. It's a long value that clutters the display so we use
+        this method to remove it """
+    ## TODO: add type checking; this method should only accept dictionaries
+    if (isinstance(flightList, dict)):
+        print('removeDataKeyFromFlights: flightList is type dict: Scrubbing data...')
+        for flight in flightList.items():
+            #flight is a tuple. Index 1 contains the flight dict where we need to delete the data key
+            del flight[1]['data']
+        return flightList
+    elif (isinstance(flightList, list)):
+        print('RemoveDataKeyFromFlights: flightList is type list: Scrubbing data...')
+        for flight in flightList:
+            #flight is a dict. Simply remove the key
+            del flight['data']
+        return flightList
+
+def getTripSummary(trip):
+    ## TODO: this only prints atm; need to format something prettier
+    print('Flight data for: ')
+    print('From: ' + trip['from']['airports'][0])
+    print('To: ' + trip['to']['airports'][0])
+
+def displayTrips(roundtrips):
+
+    flightIndex = 1
+    for trip in roundtrips['flights']:
+        print('')
+        print('Flight #' + str(flightIndex) + '---------')
+        print('OUTBOUND')
+        for segment in range(0, trip['outbound']['count']):
+            #print(str(trip['outbound']['segments'][segment]['airline']) + ' ' + str(trip['outbound']['segments'][segment]['flight_number']))
+            leg = trip['outbound']['segments'][segment]
+            displaySegmentInfo(leg)
+        print('INBOUND')
+        for segment in range(0, trip['inbound']['count']):
+            leg = trip['inbound']['segments'][segment]
+            displaySegmentInfo(leg)
+        print('Total roundtrip cost: $ ' + formatFlightCost(trip['round_trip_cost']))
+        flightIndex += 1
+    return None
+
+def formatFlightCost(priceInPennies):
+    return '${:,.2f}'.format(priceInPennies / 100)
+
+def formatDuration(durationInSeconds):
+    # input is int
+    # Example: duration = 6960 seconds = 116 mins = 1.9333 hours
+    hourValue = math.floor((durationInSeconds / 60) / 60) # equals 1.933 and then floored to 1
+    minuteValue = int((durationInSeconds / 60) % 60)
+
+    formattedValue = '{}h{}m'.format(hourValue, minuteValue)
+
+    return formattedValue
+
+def displaySegmentInfo(segment):
+
+    airline = segment['airline']
+    flight_number = segment['flight_number']
+    outboundAirport = segment['departure']['airport']
+    outboundDepTime = segment['departure']['time']
+    outboundDuration = segment['duration']
+    inboundAirpot = segment['arrival']['airport']
+    arrivalTime = segment['arrival']['time']
+
+    #Remove timezone data from times
+    outboundDepTime = outboundDepTime[:-6]
+    outboundDepTime = datetime.strptime(outboundDepTime, '%Y-%m-%dT%H:%M:%S')
+    outTime = outboundDepTime.strftime('%m/%d/%y - %H:%M')
+
+    arrivalTime = arrivalTime[:-6]
+    arrivalTime = datetime.strptime(arrivalTime, '%Y-%m-%dT%H:%M:%S')
+    inTime = arrivalTime.strftime('%H:%M')
+
+    #format duration to 2 decimal float
+    outboundDuration = formatDuration(outboundDuration)
+
+    # UA 1234 - EWR -> CHS - 8:00 -> 12:00 (Duration: 4)
+    print ('{} {} - {} -> - {} {} -> {} (Duration: {})'.format(airline, flight_number, outboundAirport, inboundAirpot,
+                                                              outTime, inTime, outboundDuration))
+
+
+
+
+
+
---
+++ FlightScraper.py
@@ -0,0 +1,79 @@
+import json
+import requests
+from datetime import datetime
+import FlightHelper as fh
+
+# Commented out to save time while testing...
+#res = requests.get('https://skiplagged.com/api/search.php?from=EWR&to=CHS&depart=2018-10-12&return=2018-10-14&format=v2&_=1519778653193',
+                   #headers = {'User-Agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.109 Safari/537.36'})
+#flightData = json.loads(res.text)
+
+
+#Below works if we ever want to read json from file instead
+with open('flightData.json') as json_file:
+    flightData = json.load(json_file)
+
+
+# BELOW WORKS
+#flightData = json.loads(res.text)
+
+
+# list of airlines found in search results
+airlines = {}
+# list of cities found in all itineraries
+cities = {}
+# list of airports found
+airports = {}
+#list of all one-way flights (both outbound and inbound)
+flights = {}
+# list of all outbound flights including the outbound price AND MIN ROUNDTRIP PRICE and flight ID
+flightsOutbound = []
+# list of all inbound flights including JUST the inbound price and flight ID
+flightsInbound = []
+# broken down into outbound and inbound flights including the one way price of the segment and flight ID
+itineraries = {}
+
+trip = {}
+
+for key, value in flightData.items():
+    if (key == 'airlines'):
+        airlines = value
+    if (key == 'cities'):
+        cities = value
+    if (key == 'airports'):
+        airports = value
+    if (key == 'flights'):
+        flights = value
+    if (key == 'itineraries'):
+        flightsOutbound = value['outbound']
+        flightsInbound = value['inbound']
+    if (key == 'info'):
+        trip = value
+
+#A little spring cleaning... remove the data value since it clutters the display
+flights = fh.removeDataKeyFromFlights(flights)
+flightsInbound = fh.removeDataKeyFromFlights(flightsInbound)
+flightsOutbound = fh.removeDataKeyFromFlights(flightsOutbound)
+
+#Sanitize the flights by removing the long flights; flights value still retains original list if necessary
+sanitizedFlights = fh.sanitizeMultiStopLongFlights(flights)
+flightsMatchQuery = sanitizedFlights[0]
+fh.setAvailableFlightList(flightsMatchQuery)
+
+flightsDontMatchQuery = sanitizedFlights[1]
+
+
+#Now sanitize the in/outbound by removing all inbound and outbound flights that are too long or have too many connections
+filteredInbound, filteredOutbound = fh.sanitizeInOutbound(flightsInbound, flightsOutbound, flightsDontMatchQuery)
+
+
+#At this point sanitizedFlights, filteredInbound, and filteredOutbound have the flight data we want to work with
+
+#print(str(fh.getCheapestRoundTripFlight(flightsOutbound, flightsInbound, flights)))
+roundTripResults = fh.getCheapestRoundTripFlights(filteredOutbound, filteredInbound)
+
+fh.displayTrips(roundTripResults)
+
+
+
+
